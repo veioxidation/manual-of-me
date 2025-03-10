7b65ed84c750001bb139e75e7a478e04d08eb07f
veioxidation <przemekdobrenko@gmail.com>
2025-03-10 18:47:29 +0100

    GItLab config

--- manual-of-me/vite.config.js
+++ manual-of-me/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/manual-of-me/', // Replace 'your-project' with your actual repo name
 })
